137067ed5eecece8c6bb2b4a044c6a292f35520f
Randalthor95 <Randalthor95@gmail.com>
2018-09-23 18:18:26 -0400

    Added methods for generating possible, substituiton, deletion, and inserstion mutations and combos

--- src/Application.java
+++ src/Application.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 public class Application {
 
     public static void main(String[] args) {
-        ArrayList<String> mutations = Testing.generate_substitution_mutation_permutations("act", 2);
-
-        for(int i = 0 ; i < mutations.size(); ++i) {
-            System.out.println(mutations.get(i));
-        }
 
     }
 }
